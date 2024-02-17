e82979a3fde91d0a21b3b8d6e12373fee1451936
Deepak Kaushal <Deepak.Kaushal@dgsl.in>
2024-02-17 17:41:26 +0530

    new page added

---
+++ src/components/pages/Privacy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function Privacy() {
+
+
+  return (
+    <div style={{
+      minHeight: 'calc(100vh - 244px)', display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h3>Page is under construction...</h3>
+    </div>
+  )
+}
